c2a133f6eac0a2bfc38d5fbda4920841992f175c
soto-raul <raul.soto@encora.com>
2025-07-22 11:26:52 -0700

    Added ToDoService implementation

---
+++ backend/src/main/java/todoapp/backend/service/ToDoService.java
@@ -0,0 +1,95 @@
+package todoapp.backend.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import todoapp.backend.enums.Status;
+import todoapp.backend.exception.InvalidToDoPropertiesException;
+import todoapp.backend.exception.ToDoNotFoundException;
+import todoapp.backend.model.FilterCriteria;
+import todoapp.backend.model.ToDo;
+import todoapp.backend.repository.ToDoInMemoRepository;
+import todoapp.backend.util.Validators;
+
+@Service
+public class ToDoService {
+
+    private final ToDoInMemoRepository toDoInMemoRepository;
+    private int nextId;
+
+    public ToDoService(ToDoInMemoRepository toDoInMemoRepository) {
+        this.toDoInMemoRepository = toDoInMemoRepository;
+        nextId = 1;
+    }
+
+    public List<ToDo> getAllToDos() {
+        return toDoInMemoRepository.findAll();
+    }
+
+    public List<ToDo> getByCriteria(FilterCriteria filterCriteria) {
+        return toDoInMemoRepository.findAllByCriteria(filterCriteria);
+    }
+
+    public ToDo addToDo(ToDo toDo) {
+        // Validate submitted new ToDo's properties
+        if (Validators.validateToDo(toDo)) {
+            // set up new ToDo's properties
+            ToDo newToDo = new ToDo();
+            newToDo.setId(nextId);
+            newToDo.setName(toDo.getName());
+            newToDo.setDueDate(toDo.getDueDate());
+            newToDo.setPriority(toDo.getPriority());
+
+            // increment nextId value
+            nextId++;
+
+            return toDoInMemoRepository.add(newToDo);
+        } else {
+            throw new InvalidToDoPropertiesException();
+        }
+
+    }
+
+    public ToDo updateToDo(int id, ToDo updatedToDo) {
+        ToDo existingToDo = toDoInMemoRepository.findById(id);
+
+        // Throw exception if no ToDo was found
+        if (existingToDo == null) {
+            throw new ToDoNotFoundException("No ToDo matching ID '" + id + "' was found.");
+        }
+
+        // Validate updatedToDo data
+        if (Validators.validateToDo(updatedToDo)) {
+            existingToDo.setName(updatedToDo.getName());
+            existingToDo.setDueDate(updatedToDo.getDueDate());
+            existingToDo.setPriority(updatedToDo.getPriority());
+            return toDoInMemoRepository.update(id, existingToDo);
+        } else {
+            throw new InvalidToDoPropertiesException();
+        }
+
+    }
+
+    public ToDo updateToDoDoneStatus(int id, Status doneStatus) {
+        ToDo existingToDo = toDoInMemoRepository.findById(id);
+
+        // Throw exception if no ToDo was found
+        if (existingToDo == null) {
+            throw new ToDoNotFoundException("No ToDo matching ID '" + id + "' was found.");
+        }
+
+        // Otherwise, update done status
+        existingToDo.setIsDone(doneStatus);
+
+        return toDoInMemoRepository.update(id, existingToDo);
+    }
+
+    public boolean deleteToDo(int id) {
+        boolean wasDeleted = toDoInMemoRepository.delete(id);
+        if (!wasDeleted) {
+            throw new ToDoNotFoundException("No ToDo matching ID '" + id + "' was found.");
+        }
+        return wasDeleted;
+    }
+}
