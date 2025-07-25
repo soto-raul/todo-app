a417042cb43b479415f0054f27dcc0e1ad346143
soto-raul <raul.soto@encora.com>
2025-07-24 18:28:54 -0700

    Updated ToDoInMemoRepository and ToDoService tests

--- backend/src/test/java/todoapp/backend/service/ToDoServiceTest.java
+++ backend/src/test/java/todoapp/backend/service/ToDoServiceTest.java
@@ -1,15 +1,14 @@
 package todoapp.backend.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -22,6 +21,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import todoapp.backend.enums.Priority;
 import todoapp.backend.enums.Status;
+import todoapp.backend.exception.ToDoNotFoundException;
 import todoapp.backend.model.FilterCriteria;
 import todoapp.backend.model.ToDo;
 import todoapp.backend.repository.ToDoInMemoRepository;
@@ -40,9 +40,9 @@ public class ToDoServiceTest {
 
     @BeforeEach
     void init() {
-        toDo1 = new ToDo(1, "Test API", LocalDateTime.of(2025, 8, 4, 12, 30), Priority.HIGH);
+        toDo1 = new ToDo(1, "Test API", LocalDate.of(2025, 8, 4), Priority.HIGH);
         toDo2 = new ToDo(2, "Write API documentation", null, Priority.LOW);
-        toDo3 = new ToDo(3, "Finish essay", LocalDateTime.of(2025, 7, 18, 15, 00), Priority.MEDIUM);
+        toDo3 = new ToDo(3, "Finish essay", LocalDate.of(2025, 7, 18), Priority.MEDIUM);
         toDo3.setIsDone(Status.DONE);
     }
 
@@ -74,16 +74,15 @@ public class ToDoServiceTest {
     @Test
     @DisplayName("Test for getByCriteria() method when filtering only by priority.")
     void testGetByPriority() {
-        Priority[] priorities = { Priority.HIGH, Priority.MEDIUM };
-        FilterCriteria criteria = new FilterCriteria(null, priorities, null);
+        FilterCriteria criteria = new FilterCriteria(null, Priority.MEDIUM, null);
 
-        when(toDoRepository.findAllByCriteria(criteria)).thenReturn(List.of(toDo1, toDo3));
+        when(toDoRepository.findAllByCriteria(criteria)).thenReturn(List.of(toDo3));
 
         List<ToDo> toDosByPriority = toDoService.getByCriteria(criteria);
 
         // Assertions
         assertNotNull(toDosByPriority);
-        assertTrue(toDosByPriority.size() == 2);
+        assertTrue(toDosByPriority.size() == 1);
     }
 
     @Test
@@ -103,8 +102,7 @@ public class ToDoServiceTest {
     @Test
     @DisplayName("Test for getByCriteria() method when filtering by all possible criteria.")
     void testGetByMultipleCriteria() {
-        Priority[] priorities = { Priority.MEDIUM };
-        FilterCriteria criteria = new FilterCriteria("Essay", priorities, Status.DONE);
+        FilterCriteria criteria = new FilterCriteria("Essay", Priority.MEDIUM, Status.DONE);
 
         when(toDoRepository.findAllByCriteria(criteria)).thenReturn(List.of(toDo3));
 
@@ -130,10 +128,11 @@ public class ToDoServiceTest {
     @Test
     @DisplayName("Test for updateToDo() method")
     void testUpdateToDoData() {
-        when(toDoRepository.update(eq(2), any(ToDo.class))).thenReturn(toDo2);
+        when(toDoRepository.findById(anyInt())).thenReturn(toDo2);
+        when(toDoRepository.update(anyInt(), any(ToDo.class))).thenReturn(toDo2);
 
         toDo2.setName("Updated ToDo Name");
-        toDo2.setDueDate(LocalDateTime.of(2025, 7, 23, 10, 00));
+        toDo2.setDueDate(LocalDate.of(2025, 7, 23));
         toDo2.setPriority(Priority.HIGH);
 
         ToDo updatedToDo = toDoService.updateToDo(2, toDo2);
@@ -143,15 +142,12 @@ public class ToDoServiceTest {
         assertEquals(updatedToDo.getId(), toDo2.getId());
     }
 
-    @Test
+    @Test()
     @DisplayName("Test for updateToDo() method when trying to update a non existing To Do object")
-    void testUpdateNonExistingToDo() {
-        when(toDoRepository.update(eq(10), any(ToDo.class))).thenReturn(null);
+    void testUpdateNonExistingToDo() throws ToDoNotFoundException {
+        when(toDoRepository.findById(anyInt())).thenReturn(null).thenThrow(ToDoNotFoundException.class);
 
-        ToDo updatedToDo = toDoService.updateToDo(10, toDo2);
-
-        // Assertions
-        assertNull(updatedToDo);
+        assertThrows(ToDoNotFoundException.class, () -> toDoService.updateToDo(10, toDo2));
     }
 
     @Test
@@ -167,12 +163,9 @@ public class ToDoServiceTest {
 
     @Test
     @DisplayName("Test for deleteToDo() method when trying to delete a non existent ")
-    void testDeleteNonExistingToDo() {
+    void testDeleteNonExistingToDo() throws ToDoNotFoundException {
         when(toDoRepository.delete(10)).thenReturn(false);
 
-        boolean wasDeleted = toDoService.deleteToDo(10);
-
-        // Assertions
-        assertFalse(wasDeleted);
+        assertThrows(ToDoNotFoundException.class, () -> toDoService.deleteToDo(10));
     }
 }
