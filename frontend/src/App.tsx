e9964475cd1bcf8957650c34db81170cee21ad06
soto-raul <raul.soto@encora.com>
2025-07-28 16:03:00 -0700

    Added metrics dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { HttpStatusCode } from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
 import FilterControls from "./components/FilterControls/FilterControls";
+import MetricsDashboard from "./components/MetricsDashboard/MetricsDashboard";
 import Paginator from "./components/Paginator/Paginator";
 import ToDoList from "./components/ToDoList/ToDoList";
 import ToDoModalForm from "./components/ToDoModalForm/ToDoModalForm";
@@ -9,6 +10,7 @@ import {
   addToDo,
   deleteToDo,
   getAllToDos,
+  getMetrics,
   markAsDone,
   markAsUndone,
   updateToDo,
@@ -53,6 +55,11 @@ function App() {
   const [sortingCriteria, setSortingCriteria] = useState<
     Map<string, SortingOrder>
   >(new Map<string, SortingOrder>());
+
+  // Metrics
+  const [metrics, setMetrics] = useState<Map<string, number>>(
+    new Map<string, number>()
+  );
 
   useEffect(() => {
     // if there are any filters applied and we're not in the first page of the list, first reset pagination
@@ -99,6 +106,10 @@ function App() {
     promise.then((response) => {
       if (response?.status === HttpStatusCode.Ok) {
         fetchToDos();
+
+        if (isToDo(formData)) {
+          handleUpdateMetrics();
+        }
       } else {
         console.error(response?.data);
       }
@@ -113,6 +124,7 @@ function App() {
       .then((response) => {
         if (response?.status === HttpStatusCode.Ok) {
           fetchToDos();
+          handleUpdateMetrics();
         }
       })
       .catch((error) => {
@@ -176,6 +188,17 @@ function App() {
     fetchToDos();
   };
 
+  // handles updating metrics
+  const handleUpdateMetrics = () => {
+    getMetrics()
+      .then((response) => {
+        setMetrics(new Map(Object.entries(response?.data)));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <>
       <main>
@@ -208,6 +231,7 @@ function App() {
           pagination={paginationData}
           onPageChange={handlePagination}
         />
+        <MetricsDashboard metricsData={metrics} />
       </main>
     </>
   );
